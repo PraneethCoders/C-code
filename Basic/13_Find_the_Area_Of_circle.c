70427c2a8da2b569ced94bef0173f172022fd286
Praneeth.Y <praneethcoders@outlook.in>
2026-06-06 10:04:08 +0530

    Simplify area calculation and print statement

--- Basic/13_Find_the_Area_Of_circle.c
+++ Basic/13_Find_the_Area_Of_circle.c
@@ -4,7 +4,7 @@ int main(){
     float r,a;
     printf("enter the radius:\n");
     scanf("%f",&r);
-    a = PI * r * r;
-    printf("Area of Circle is %.2f",a);
+    
+    printf("Area of Circle is %.2f",a = PI * r * r);
     return 0;
 }
